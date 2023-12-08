44e507675e04eaaec5f922b1b53b5e0a31602272
drweb_000 <drweb_000@user-pc>
2023-12-08 08:41:15 +0300

    added plan_entry->moveUp and plan_entry->moveDown for changign entries sort_order

--- src/data/plan_entry.php
+++ src/data/plan_entry.php
@@ -3,6 +3,8 @@
 namespace losthost\WeekTimerModel\data;
 use losthost\DB\DBObject;
 use losthost\DB\DBView;
+use losthost\DB\DBList;
+use losthost\DB\DB;
 
 class plan_entry extends DBObject {
     
@@ -33,4 +35,71 @@ class plan_entry extends DBObject {
             $this->__data['time_planned'] = 0;
         }
     }
+    
+    public function moveUp() {
+        
+        $upper_neighbor = $this->getUpperNeighbor();
+        DB::beginTransaction();
+
+        if ( $upper_neighbor ) {
+            $tmp = $this->sort_order;            
+            $this->sort_order = $upper_neighbor->sort_order;            
+            $upper_neighbor->sort_order = $tmp;
+
+            $upper_neighbor->write();
+            $this->write();
+        } else {
+            $the_rest = new DBList(static::class, 'plan = ? AND id <> ? AND sort_order < 2000000000 ORDER BY sort_order', [$this->plan, $this->id]);
+            $last_sort_order = $this->sort_order;
+            while ($entry = $the_rest->next()) {
+                $tmp = $entry->sort_order;
+                $entry->sort_order = $last_sort_order;
+                $last_sort_order = $tmp;
+                $entry->write();
+            }
+            $this->sort_order = $last_sort_order;
+            $this->write();
+        }
+        DB::commit();
+        
+    }
+    
+    public function moveDown() {
+    
+        $lower_neighbor = $this->getLowerNeighbor();
+//        DB::beginTransaction();
+
+        if ( $lower_neighbor ) {
+            $tmp = $this->sort_order;            
+            $this->sort_order = $lower_neighbor->sort_order;            
+            $lower_neighbor->sort_order = $tmp;
+            
+            $lower_neighbor->write();
+            $this->write();
+        } else {
+            $the_rest = new DBList(static::class, 'plan = ? AND id <> ? AND sort_order < 2000000000 ORDER BY sort_order DESC', [$this->plan, $this->id]);
+            $last_sort_order = $this->sort_order;
+            while ($entry = $the_rest->next()) {
+                $tmp = $entry->sort_order;
+                $entry->sort_order = $last_sort_order;
+                $last_sort_order = $tmp;
+                $entry->write();
+            }
+            $this->sort_order = $last_sort_order;
+            $this->write();
+        }
+//        DB::commit();
+        
+    }
+    
+    protected function getUpperNeighbor() {
+        $the_neighbor = new DBList(static::class, 'plan = ? AND sort_order < ? ORDER BY sort_order DESC LIMIT 1', [$this->plan, $this->sort_order]);
+        return $the_neighbor->next();
+    }
+    
+    protected function getLowerNeighbor() {
+        $the_neighbor = new DBList(static::class, 'plan = ? AND sort_order > ? AND sort_order < 2000000000 ORDER BY sort_order LIMIT 1', [$this->plan, $this->sort_order]);
+        return $the_neighbor->next();
+    }
+    
 }
--- test/plan_entryTest.php
+++ test/plan_entryTest.php
@@ -3,15 +3,47 @@
 namespace losthost\WeekTimerModel\test;
 use PHPUnit\Framework\TestCase;
 use losthost\WeekTimerModel\data\plan_entry;
+use losthost\WeekTimerModel\data\user;
+use losthost\WeekTimerModel\data\plan;
+use losthost\DB\DBList;
 
 
 class plan_entryTest extends TestCase {
     
     public function testCanCreateEntry() {
         
-        // creating plan entries is tested while a new user creation
+        $user = new user();
+        $user->write();
+        $entries = new DBList(plan_entry::class, ['plan' => $user->active_plan]);
         
-        $this->assertTrue(true);
+        $this->assertInstanceOf(plan_entry::class, $entries->asArray()[0]);
         
+    }
+    
+    public function testMovingPlanEntry() {
+
+        $user = new user();
+        $user->write();
+        $entries0 = (new DBList(plan_entry::class, 'plan = ? ORDER BY sort_order', [$user->active_plan]))->asArray();
+        
+        $entries0[0]->moveDown();
+        $entries0[3]->moveDown();
+        $entries1 = (new DBList(plan_entry::class, 'plan = ? ORDER BY sort_order', [$user->active_plan]))->asArray();
+        
+        $this->assertEquals($entries0[0]->id, $entries1[2]->id);
+        $this->assertEquals($entries0[1]->id, $entries1[1]->id);
+        $this->assertEquals($entries0[2]->id, $entries1[3]->id);
+        $this->assertEquals($entries0[3]->id, $entries1[0]->id);
+        $this->assertEquals($entries0[4]->id, $entries1[4]->id);
+                                
+        $entries1[3]->moveUp(); 
+        $entries1[0]->moveUp(); 
+        $entries2 = (new DBList(plan_entry::class, 'plan = ? ORDER BY sort_order', [$user->active_plan]))->asArray();
+        
+        $this->assertEquals($entries1[0]->id, $entries2[3]->id);
+        $this->assertEquals($entries1[1]->id, $entries2[0]->id);
+        $this->assertEquals($entries1[2]->id, $entries2[2]->id);
+        $this->assertEquals($entries1[3]->id, $entries2[1]->id);
+        $this->assertEquals($entries1[4]->id, $entries2[4]->id);
     }
 }
